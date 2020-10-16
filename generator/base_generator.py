02400addcbf779c934a3e295623c40fe5ac4bbb8
NevMem <memlolkek@gmail.com>
2020-10-17 02:02:50 +0300

    Filling start times

--- generator/base_generator.py
+++ generator/base_generator.py
@@ -34,7 +34,40 @@ class DSU:
         return self.components
 
 def fill_start_poins(tasks):
-    pass
+    queue = []
+    blocker_to = dict()
+    for task in tasks:
+        if len(task.depends_on):
+            queue.append(task.id) # using that id == index in the list (data design leak, but fine for now)
+        for depends in task.depends_on:
+            if depends in blocker_to:
+                blocker_to[depends].add(task.id)
+            else:
+                blocker_to[depends] = set([task.id])
+    
+    used_links = dict()
+    max_blocker_time_values = dict()
+
+    for i in range(len(tasks)):
+        used_links[i] = 0
+        max_blocker_time_values[i] = 0
+
+    while len(queue) != 0:
+        task_id = queue[0]
+        queue = queue[1:]
+
+        if not task_id in blocker_to:
+            continue
+
+        for blocked in blocker_to[task_id]:
+            used_links[blocked] += 1
+            max_blocker_time_values[blocked] = max(
+                max_blocker_time_values[blocked],
+                tasks[task_id].start + tasks[task_id].time)
+            if used_links[blocked] == len(tasks[blocked].depends_on):
+                queue.append(blocked)
+                tasks[blocked].start = max_blocker_time_values[blocked] + 1
+
 
 def create_graph_metadata(tasks):
     count_start_tasks = 0
@@ -46,9 +79,16 @@ def create_graph_metadata(tasks):
             is_blocker.add(blocked)
     count_end_tasks = len(tasks) - len(is_blocker)
 
+    critical_path_time_length = 0
+    for task in tasks:
+        critical_path_time_length = max(
+            critical_path_time_length,
+            task.start + task.time)
+
     return {
         "start_tasks": count_start_tasks,
-        "end_tasks": count_end_tasks
+        "end_tasks": count_end_tasks,
+        "critical_path_time_length": critical_path_time_length
     }
 
 class BaseGenerator(Generator):
@@ -104,12 +144,12 @@ class BaseGenerator(Generator):
             assert(required < blocked)
             tasks[blocked].add_depends_on(required)
 
+        fill_start_poins(tasks)
+
         if print_metadata:
             metadata = create_graph_metadata(tasks)
             for field in metadata:
                 print(field, ':', metadata[field])
-
-        fill_start_poins(tasks)
 
         return tasks
 
