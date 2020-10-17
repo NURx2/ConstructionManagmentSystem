cd6eb8f66a41eff95f5580bda12b21fc0726b8cc
NevMem <memlolkek@gmail.com>
2020-10-17 19:22:10 +0300

    DiffReport

---
+++ data/Diff.py
@@ -0,0 +1,20 @@
+from data.Task import Task
+
+def calculate_move_cost(before: Task, after: Task) -> int:
+    if before.start < after.start:
+        return before.costs['moveForward'] * (after.start - before.start)
+    return before.costs['moveBack'] * (before.start - after.start)
+
+def calculate_shrink_cost(before: Task, after: Task):
+    assert(before.time > after.time)
+    return before.lower_time * (before.time - after.time)
+
+class Diff:
+    def __init__(self, before: Task, after: Task):
+        assert(before != after)
+        assert(before.id == after.id)
+        self.was_moved = before.start != after.start
+        self.was_shrinked = before.time != after.time
+        self.move_cost = None if not self.was_moved else calculate_move_cost(before, after)
+        self.shrink_cost = None if not self.was_shrinked else calculate_shrink_cost(before, after)
+        self.whole_cost = self.move_cost + self.shrink_cost
---
+++ data/DiffReport.py
@@ -0,0 +1,10 @@
+from data.Diff import Diff
+
+class DiffReport:
+    def __init__(self, diff_list: List[Diff], was_collisions: int, now_collisions):
+        self.diff_list = diff_list
+        self.was_collisions = was_collisions
+        self.now_collisions = now_collisions
+        self.whole_cost = 0
+        for diff in self.diff_list:
+            self.whole_cost += diff.whole_cost
