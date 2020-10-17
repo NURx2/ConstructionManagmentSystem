a688673b0fefff61bd435786ebf2d1b13ab5f3bb
NevMem <memlolkek@gmail.com>
2020-10-17 05:18:35 +0300

    CollisionGenerator

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from generator.BaseGenerator import BaseGenerator
 from validation.CollisionDetector import CollisionDetector
+from collision.DefaultCollisionGenerator import DefaultCollisionGenerator
 
 
 def main():
     data = BaseGenerator().generate_data()
     detector = CollisionDetector()
-    print('Collisions:', len(detector.get_collisions(data)))
     print(len(data))
     assert(len(detector.get_collisions(data)) == 0)
-    for collision in detector.get_collisions(data):
-        first, second = collision.first_task_id, collision.second_task_id
-        print(first, second)
-        print(data[first].start + data[first].time, data[second].time)
+
+    collision_generator = DefaultCollisionGenerator()
+    new_tasks = collision_generator.generate_collisions(data)
+    assert(len(detector.get_collisions(data)) == 0)
+    assert(len(detector.get_collisions(new_tasks)) != 0)
 
 if __name__ == '__main__':
     main()
