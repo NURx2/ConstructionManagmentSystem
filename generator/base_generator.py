4dd4faac34fa5c2d1119b40540f6379d6056fa05
NevMem <memlolkek@gmail.com>
2020-10-17 04:21:04 +0300

    Bug fix + CollisionDetector

--- generator/base_generator.py
+++ generator/base_generator.py
@@ -37,7 +37,7 @@ def fill_start_poins(tasks):
     queue = []
     blocker_to = dict()
     for task in tasks:
-        if len(task.depends_on):
+        if len(task.depends_on) == 0:
             queue.append(task.id) # using that id == index in the list (data design leak, but fine for now)
         for depends in task.depends_on:
             if depends in blocker_to:
@@ -47,6 +47,7 @@ def fill_start_poins(tasks):
     
     used_links = dict()
     max_blocker_time_values = dict()
+    visited = set()
 
     for i in range(len(tasks)):
         used_links[i] = 0
@@ -55,6 +56,9 @@ def fill_start_poins(tasks):
     while len(queue) != 0:
         task_id = queue[0]
         queue = queue[1:]
+
+        assert(task_id not in visited)
+        visited.add(task_id)
 
         if not task_id in blocker_to:
             continue
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from generator.base_generator import BaseGenerator
+from validation.CollisionDetector import CollisionDetector
 
 
 def main():
     data = BaseGenerator().generate_data()
+    detector = CollisionDetector()
+    print('Collisions:', len(detector.get_collisions(data)))
     print(len(data))
+    for collision in detector.get_collisions(data):
+        first, second = collision.first_task_id, collision.second_task_id
+        print(first, second)
+        print(data[first].start + data[first].time, data[second].time)
 
 if __name__ == '__main__':
     main()
