7c7f2579a5ea082caff15dee7ecca24a33dabc00
NevMem <memlolkek@gmail.com>
2020-10-17 14:14:16 +0300

    More List's

--- solution/JustCopySolution.py
+++ solution/JustCopySolution.py
@@ -1,7 +1,9 @@
+from data.Task import Task
 from solution.Solution import Solution
+from typing import List
 
 class JustCopySolution(Solution):
-    def solve(self, tasks): # we need to copy tasks (to make previous tasks immutable)
+    def solve(self, tasks: List[Task]) -> List[Task]: # we need to copy tasks (to make previous tasks immutable)
         result = []
         for task in tasks:
             result.append(task.copy())
